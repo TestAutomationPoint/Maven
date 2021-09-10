654a49996f57ab878732582c3169753619e87cdd
ravimuduthana <ravimuduthana@gmail.com>
2021-09-10 13:58:55 +0530

    changes

---
+++ src/test/java/scripts/AppiumTest.java
@@ -0,0 +1,15 @@
+package scripts;
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	
+
+	@Test
+	public void executeMobilescript()
+	{
+		System.out.println("executing Mobilescript");
+	}
+
+}
+
---
+++ src/test/java/scripts/SeleniumTest.java
@@ -0,0 +1,15 @@
+package scripts;
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	
+
+	@Test
+	public void executescript()
+	{
+		System.out.println("executing script");
+	}
+
+}
+
